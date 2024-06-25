2dc53ea71cfaf0817d601d9b5a04f85aab776312
Cenote <himangshukalita131@gmail.com>
2024-06-25 21:41:08 +0530

    Upto delete note api route

--- backend/controller/note.controller.js
+++ backend/controller/note.controller.js
@@ -32,3 +32,87 @@ export const addNote = async (req, res, next) => {
     next(error)
   }
 }
+
+export const editNote = async (req, res, next) => {
+  const note = await Note.findById(req.params.noteId)
+
+  //   console.log(req.params.noteId)
+
+  if (!note) {
+    return next(errorHandler(404, "Note not found!"))
+  }
+
+  if (req.user.id !== note.userId) {
+    return next(errorHandler(401, "You can only update your own note!"))
+  }
+
+  const { title, content, tags, isPinned } = req.body
+
+  if (!title && !content && !tags) {
+    return next(errorHandler(404, "No Changes provided!"))
+  }
+
+  try {
+    if (title) {
+      note.title = title
+    }
+
+    if (content) {
+      note.content = content
+    }
+
+    if (tags) {
+      note.tags = tags
+    }
+
+    if (isPinned) {
+      note.isPinned = isPinned
+    }
+
+    await note.save()
+
+    res.status(200).json({
+      success: true,
+      message: "Note updated successfully",
+      note,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getAllNotes = async (req, res, next) => {
+  const userId = req.user.id
+
+  try {
+    const notes = await Note.find({ userId: userId }).sort({ isPinned: -1 })
+
+    res.status(200).json({
+      success: true,
+      message: "All notes retrived successfully",
+      notes,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const deleteNote = async (req, res, next) => {
+  const noteId = req.params.noteId
+  const note = await Note.findOne({ _id: noteId, userId: req.user.id })
+
+  if (!note) {
+    return next(errorHandler(404, "Note not found!"))
+  }
+
+  try {
+    await Note.deleteOne({ _id: noteId, userId: req.user.id })
+
+    res.status(200).json({
+      success: true,
+      message: "Note deleted successfully",
+    })
+  } catch (error) {
+    next(error)
+  }
+}
--- backend/routes/note.route.js
+++ backend/routes/note.route.js
@@ -1,9 +1,17 @@
 import express from "express"
 import { verifyToken } from "../utils/varifyUser.js"
-import { addNote } from "../controller/note.controller.js"
+import {
+  addNote,
+  deleteNote,
+  editNote,
+  getAllNotes,
+} from "../controller/note.controller.js"
 
 const router = express.Router()
 
-router.post("/add-note", verifyToken, addNote)
+router.post("/add", verifyToken, addNote)
+router.put("/edit/:noteId", verifyToken, editNote)
+router.get("/all", verifyToken, getAllNotes)
+router.delete("/delete/:noteId", verifyToken, deleteNote)
 
 export default router
