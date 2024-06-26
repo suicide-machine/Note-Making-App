b80712a4ce8bdc045cc33b78ff6ad8aa08ab45cd
Cenote <himangshukalita131@gmail.com>
2024-06-27 03:22:19 +0530

    Some issue fixed

--- frontend/src/components/Cards/ProfileInfo.jsx
+++ frontend/src/components/Cards/ProfileInfo.jsx
@@ -4,19 +4,23 @@ import { getInitials } from "../../utils/helper"
 const ProfileInfo = ({ onLogout, userInfo }) => {
   return (
     <div className="flex items-center gap-3">
-      <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
-        {getInitials(userInfo?.username)}
-      </div>
+      {userInfo && (
+        <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
+          {getInitials(userInfo?.username)}
+        </div>
+      )}
 
-      <div>
-        <p className="text-sm font-medium">{userInfo?.username}</p>
-        <button
-          className="text-sm  bg-red-500 p-1 rounded-md text-white hover:opacity-80"
-          onClick={onLogout}
-        >
-          Logout
-        </button>
-      </div>
+      {userInfo && (
+        <div>
+          <p className="text-sm font-medium">{userInfo?.username}</p>
+          <button
+            className="text-sm  bg-red-500 p-1 rounded-md text-white hover:opacity-80"
+            onClick={onLogout}
+          >
+            Logout
+          </button>
+        </div>
+      )}
     </div>
   )
 }
